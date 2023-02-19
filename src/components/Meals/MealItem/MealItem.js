452a4d549c3dfcce10b60e752679fa0f76fff207
Sergei Shatohin <trapjkeeee@gmail.com>
2023-02-19 14:10:20 +0300

    исправил баг с лэйблом

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -11,7 +11,7 @@ const MealItem = (props) => {
         <div className={styles.price}>{formattedPrice}</div>
       </div>
       <div>
-        <MealItemForm />
+        <MealItemForm id={props.id} />
       </div>
     </li>
   );
